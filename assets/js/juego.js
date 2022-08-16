83894446f1497bbdcb1f6f270d0807ef05ad9e89
Almorox13 <ralmoroxg@gmail.com>
2022-08-16 21:13:36 +0200

    valor carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -2,7 +2,8 @@ let deck = [];
 let types = ["C", "H", "D", "S"];
 let specials = ["A", "J", "Q", "K"];
 
-const createDeck = () => {
+//Creacion de una baraja desordenada
+const crearDeck = () => {
   for (let i = 2; i <= 10; i++) {
     for (const type of types) {
       deck.push(i + type);
@@ -16,5 +17,18 @@ const createDeck = () => {
   deck = _.shuffle(deck);
 };
 
-createDeck();
+//Funcion para dar una carta al usuario
+const pedirCarta = () => deck === 0 ? 'No quedan cartas en la baraja' : deck.pop();
+
+//Valor de las cartas
+const valorCarta = (carta) => { 
+
+  const valor = carta.substring(0, carta.length - 1);
+  return (isNaN(valor)) ? 
+          (valor === 'A') ? 11 : 10 : valor * 1;
+}
+
+crearDeck();
+console.log({...deck});
+console.log(valorCarta(pedirCarta()));
 console.log(deck);
