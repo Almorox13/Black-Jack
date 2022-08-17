363d64ebd53a4040a3112b345fc019ccb47bb035
Almorox13 <ralmoroxg@gmail.com>
2022-08-17 18:31:30 +0200

    logica general juego

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,16 +1,36 @@
 let deck = [];
-let types = ["C", "H", "D", "S"];
-let specials = ["A", "J", "Q", "K"];
+let puntosJugador = 0;
+let puntosCrupier = 0;
+const types = ["C", "H", "D", "S"];
+const specials = ["A", "J", "Q", "K"];
+
+const btnNuevo = document.querySelector("#btnNuevo");
+const btnPedir = document.querySelector("#btnPedir");
+const btnDetener = document.querySelector("#btnDetener");
+
+const txtJugador = document.querySelector("#txtJugador");
+const txtCrupier = document.querySelector("#txtCrupier");
+
+const divCartaJugador = document.querySelector("#cartas-jugador");
+const divCartaCrupier = document.querySelector("#cartas-crupier");
+
+const rutaImagenes = "assets/img/";
+const extensionImg = ".png";
 
 //Creacion de una baraja desordenada
 const crearDeck = () => {
+
   for (let i = 2; i <= 10; i++) {
+
     for (const type of types) {
+
       deck.push(i + type);
     }
   }
   for (const special of specials) {
+
     for (const type of types) {
+
       deck.push(special + type);
     }
   }
@@ -18,17 +38,113 @@ const crearDeck = () => {
 };
 
 //Funcion para dar una carta al usuario
-const pedirCarta = () => deck === 0 ? 'No quedan cartas en la baraja' : deck.pop();
+const pedirCarta = () =>
+
+  deck === 0 ? 'No quedan cartas en la baraja' : (carta = deck.pop());
 
 //Valor de las cartas
-const valorCarta = (carta) => { 
+const valorCarta = (carta) => {
 
   const valor = carta.substring(0, carta.length - 1);
-  return (isNaN(valor)) ? 
-          (valor === 'A') ? 11 : 10 : valor * 1;
+  return isNaN(valor) ? (valor === 'A' ? 11 : 10) : valor * 1;
+};
+
+//Pintar una carta
+const pintarCarta = (carta, jugador) => {
+
+  let img = document.createElement('img');
+  img.src = rutaImagenes + carta + extensionImg;
+  img.className = "carta";
+
+  if (jugador === 0) {
+
+    divCartaJugador.append(img);
+  } else {
+
+    divCartaCrupier.append(img);
+  }
+};
+
+//Comprobacion de puntos
+const comprobarPuntosJugador = () => {
+
+  if(puntosJugador > 21){
+
+    txtJugador.style.color = 'red';
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    puntosJugador = 0.1;
+    turnoCrupier();
+  }
+  if(puntosJugador == 21){
+
+    txtJugador.style.color = 'blue';
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+    turnoCrupier();
+  }
+};
+
+const comprobarPuntosCrupier = () => {
+
+  console.log(puntosCrupier);
+
+  if(puntosCrupier > 21){
+
+    txtCrupier.style.color = 'red';
+  }else{
+    if((puntosCrupier > puntosJugador) || (puntosCrupier == 21)){
+
+      txtCrupier.style.color = 'blue';
+    }
+    if(puntosCrupier == puntosJugador){
+
+      txtCrupier.style.color = 'yellow';
+      txtJugador.style.color = 'yellow';
+    }
+  }
 }
 
+const turnoCrupier = () => {
+
+  while((puntosJugador > puntosCrupier) && (puntosCrupier < 21)){
+
+    let carta = pedirCarta();
+    pintarCarta(carta, 1);
+    puntosCrupier = puntosCrupier + valorCarta(carta);
+    txtCrupier.innerHTML = puntosCrupier;
+  }
+  comprobarPuntosCrupier();
+};
+
+//Evento pedir carta
+btnPedir.addEventListener("click", () => {
+
+  const carta = pedirCarta();
+  pintarCarta(carta, 0);
+  puntosJugador = puntosJugador + valorCarta(carta);
+  txtJugador.innerHTML = puntosJugador;
+  comprobarPuntosJugador();
+});
+
+//Evento nuevo juego
+btnNuevo.addEventListener('click', () => {
+
+  while(divCartaJugador.firstChild){
+    divCartaJugador.removeChild(divCartaJugador.lastChild);
+  }
+  while(divCartaCrupier.firstChild){
+    divCartaCrupier.removeChild(divCartaCrupier.lastChild);
+  }
+  puntosJugador = 0;
+  puntosCrupier = 0;
+  txtCrupier.innerHTML = puntosCrupier;
+  txtJugador.innerHTML = puntosJugador;
+  btnPedir.disabled = false;
+  btnDetener.disabled = false;
+  txtJugador.style.color = 'white';
+  txtCrupier.style.color = 'white';
+  crearDeck();
+});
+
 crearDeck();
-console.log({...deck});
-console.log(valorCarta(pedirCarta()));
-console.log(deck);
