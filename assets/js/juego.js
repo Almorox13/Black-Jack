a08eb252a8355e17045ce8431fc6821dd96dd451
Almorox13 <ralmoroxg@gmail.com>
2022-09-29 14:41:34 +0200

    Optimizacion

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,141 +1,159 @@
-let deck = [];
-let puntosJugador = 0;
-let puntosCrupier = 0;
+(() => {
 
-const types = ["C", "H", "D", "S"];
-const specials = ["A", "J", "Q", "K"];
+  let deck = [],
+      puntosJugador = 0,
+      puntosCrupier = 0;
 
-const btnNuevo = document.querySelector("#btnNuevo");
-const btnPedir = document.querySelector("#btnPedir");
-const btnDetener = document.querySelector("#btnDetener");
+  const types = ["C", "H", "D", "S"],
+        specials = ["A", "J", "Q", "K"];
 
-const txtJugador = document.querySelector("#txtJugador");
-const txtCrupier = document.querySelector("#txtCrupier");
+  const btnNuevo = document.querySelector("#btnNuevo"),
+        btnPedir = document.querySelector("#btnPedir"),
+        btnDetener = document.querySelector("#btnDetener");
 
-const divCartaJugador = document.querySelector("#cartas-jugador");
-const divCartaCrupier = document.querySelector("#cartas-crupier");
+  const txtJugador = document.querySelector("#txtJugador"),
+        txtCrupier = document.querySelector("#txtCrupier");
 
-const rutaImagenes = "assets/img/";
-const extensionImg = ".png";
+  const divCartas = document.querySelectorAll('.divCartas');
+        
+  const rutaImagenes = "assets/img/";
+        extensionImg = ".png";
 
-//Creacion de una baraja desordenada
-const crearDeck = () => {
-  for (let i = 2; i <= 10; i++) {
-    for (const type of types) {
-      deck.push(i + type);
+  //Funcion para inicializar el juego
+  const inicializar = () => {
+    deck = crearDeck();
+  }
+
+  //Creacion de una baraja desordenada
+  const crearDeck = () => {
+
+    deck = [];
+
+    for (let i = 2; i <= 10; i++) {
+      for (const type of types) {
+        deck.push(i + type);
+      }
     }
-  }
-  for (const special of specials) {
-    for (const type of types) {
-      deck.push(special + type);
+    for (const special of specials) {
+      for (const type of types) {
+        deck.push(special + type);
+      }
     }
-  }
-  deck = _.shuffle(deck);
-};
+    return _.shuffle(deck);
+  };
 
-//Funcion para dar una carta al usuario
-const pedirCarta = () =>
-  deck === 0 ? "No quedan cartas en la baraja" : (carta = deck.pop());
+  //Funcion para dar una carta al usuario
+  const pedirCarta = () =>
+    deck === (0 || undefined) ? "No quedan cartas en la baraja" : (carta = deck.pop());
 
-//Valor de las cartas
-const valorCarta = (carta) => {
-  const valor = carta.substring(0, carta.length - 1);
-  return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
-};
+  //Valor de las cartas
+  const valorCarta = (carta) => {
+    const valor = carta.substring(0, carta.length - 1);
+    return isNaN(valor) ? (valor === "A" ? 11 : 10) : valor * 1;
+  };
 
-//Pintar una carta
-const pintarCarta = (carta, jugador) => {
-  let img = document.createElement("img");
-  img.src = rutaImagenes + carta + extensionImg;
-  img.className = "carta";
+  //Pintar una carta
+  const pintarCarta = (carta, jugador) => {
+    let img = document.createElement("img");
+    img.src = rutaImagenes + carta + extensionImg;
+    img.className = "carta";
 
-  if (jugador === 0) {
-    divCartaJugador.append(img);
-  } else {
-    divCartaCrupier.append(img);
-  }
-};
+    if (jugador === 0) {
+      divCartas[0].append(img);
+    } else {
+      divCartas[1].append(img);
+    }
+  };
 
-//Comprobacion de puntos del jugador
-const comprobarPuntosJugador = () => {
-  if (puntosJugador > 21) {
-    txtJugador.style.color = "red";
-    btnPedir.disabled = true;
-    btnDetener.disabled = true;
-    puntosJugador = 0.1;
-    turnoCrupier();
-  }
-  if (puntosJugador == 21) {
-    txtJugador.style.color = "blue";
-    btnPedir.disabled = true;
-    btnDetener.disabled = true;
-    turnoCrupier();
-  }
-};
-
-//Comprobacion de los puntos del crupier
-const comprobarPuntosCrupier = () => {
-  console.log(puntosCrupier);
-
-  if (puntosCrupier > 21) {
-    txtCrupier.style.color = "red";
-    txtJugador.style.color = "blue";
-  } else {
-    if (puntosCrupier > puntosJugador || puntosCrupier == 21) {
-      txtCrupier.style.color = "blue";
+  //Comprobacion de puntos del jugador
+  const comprobarPuntosJugador = () => {
+    if (puntosJugador > 21) {
       txtJugador.style.color = "red";
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      puntosJugador = 0.1;
+      turnoCrupier();
     }
-    if (puntosCrupier == puntosJugador) {
-      txtCrupier.style.color = "yellow";
-      txtJugador.style.color = "yellow";
+    if (puntosJugador == 21) {
+      txtJugador.style.color = "blue";
+      btnPedir.disabled = true;
+      btnDetener.disabled = true;
+      turnoCrupier();
     }
-  }
-};
+  };
 
-//Turno del crupier
-const turnoCrupier = () => {
-  while (puntosJugador >= puntosCrupier && puntosCrupier < 21) {
-    let carta = pedirCarta();
-    pintarCarta(carta, 1);
-    puntosCrupier = puntosCrupier + valorCarta(carta);
+  //Comprobacion de los puntos del crupier
+  const comprobarPuntosCrupier = () => {
+
+    if (puntosCrupier > 21) {
+      txtCrupier.style.color = "red";
+      txtJugador.style.color = "blue";
+    } else {
+      if (puntosCrupier > puntosJugador || puntosCrupier == 21) {
+        txtCrupier.style.color = "blue";
+        txtJugador.style.color = "red";
+      }
+      if (puntosCrupier == puntosJugador) {
+        txtCrupier.style.color = "yellow";
+        txtJugador.style.color = "yellow";
+      }
+    }
+  };
+
+  //Borramos las cartas de la mesa
+  const borrarCartas = () => {
+
+    while (divCartas.firstChild) {
+      divCartas.removeChild(divCartas.lastChild);
+    }
+   
+  }
+
+  //Turno del crupier
+  const turnoCrupier = () => {
+    while (puntosJugador >= puntosCrupier && puntosCrupier < 21) {
+      let carta = pedirCarta();
+      pintarCarta(carta, 1);
+      puntosCrupier = puntosCrupier + valorCarta(carta);
+      txtCrupier.innerHTML = puntosCrupier;
+    }
+    comprobarPuntosCrupier();
+  };
+
+  //Evento pedir carta
+  btnPedir.addEventListener("click", () => {
+    const carta = pedirCarta();
+    if(carta != "No quedan cartas en la baraja"){
+      pintarCarta(carta, 0);
+      puntosJugador = puntosJugador + valorCarta(carta);
+      txtJugador.innerHTML = puntosJugador;
+      comprobarPuntosJugador();
+    }else{
+      alert("Baraja vacía");
+    }
+  });
+
+  //Evento nuevo juego
+  btnNuevo.addEventListener("click", () => {
+    
+    borrarCartas();
+    puntosJugador = 0;
+    puntosCrupier = 0;
     txtCrupier.innerHTML = puntosCrupier;
-  }
-  comprobarPuntosCrupier();
-};
+    txtJugador.innerHTML = puntosJugador;
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
+    txtJugador.style.color = "white";
+    txtCrupier.style.color = "white";
+    crearDeck();
+  });
 
-//Evento pedir carta
-btnPedir.addEventListener("click", () => {
-  const carta = pedirCarta();
-  pintarCarta(carta, 0);
-  puntosJugador = puntosJugador + valorCarta(carta);
-  txtJugador.innerHTML = puntosJugador;
-  comprobarPuntosJugador();
-});
+  //Evento de plantarse
+  btnDetener.addEventListener("click", () => {
+    turnoCrupier();
+    btnPedir.disabled = true;
+    btnDetener.disabled = true;
+  });
 
-//Evento nuevo juego
-btnNuevo.addEventListener("click", () => {
-  while (divCartaJugador.firstChild) {
-    divCartaJugador.removeChild(divCartaJugador.lastChild);
-  }
-  while (divCartaCrupier.firstChild) {
-    divCartaCrupier.removeChild(divCartaCrupier.lastChild);
-  }
-  puntosJugador = 0;
-  puntosCrupier = 0;
-  txtCrupier.innerHTML = puntosCrupier;
-  txtJugador.innerHTML = puntosJugador;
-  btnPedir.disabled = false;
-  btnDetener.disabled = false;
-  txtJugador.style.color = "white";
-  txtCrupier.style.color = "white";
-  crearDeck();
-});
-
-//Evento de plantarse
-btnDetener.addEventListener("click", () => {
-  turnoCrupier();
-  btnPedir.disabled = true;
-  btnDetener.disabled = true;
-});
-
-crearDeck();
+  inicializar();
+})();
